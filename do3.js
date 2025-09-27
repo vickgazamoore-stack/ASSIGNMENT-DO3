dcaaf75457ca02d7244341a6665b6d4530e336e3
gaza-moore <vickgazamoore@gmail.com>
2025-09-27 18:38:37 +0100

    result

--- do3.js
+++ do3.js
@@ -1,42 +1,51 @@
-
-
-
 function calculate(event) {
-   event.preventDefault();  // stops form from refreshing
+  event.preventDefault(); // stops form from refreshing
 
-   let weight = Number(document.getElementById("weight").value);
-   let destination = document.getElementById("destination").value;
-   let expressDelivery = document.getElementById("express").checked;
+  let weight = Number(document.getElementById("weight").value);
+  let destination = document.getElementById("destination").value;
+  let expressDelivery = document.getElementById("express").checked;
 
-   let baseCost = 40;  // starting price
-   console.log("Start base cost:", baseCost);
+  // checking the inputs to avoid nagetive number
+  if (isNaN(weight) || weight <= 0) {
+    alert("Please enter a valid weight greater than 0.");
+    return;
+  }
 
-   // Add cost based on destination
-   if (destination === "aba") {
-      baseCost += 10;
-   } else if (destination === "bende") {
-      baseCost += 25;
-   } else if (destination === "ohafia") {
-      baseCost += 35;
-   } else if (destination === "ukwa-east") {
-      baseCost += 15;
-   } else if (destination === "ukwa-west") {
-      baseCost += 20;
-   } else if (destination === "umuahia") {
-      baseCost += 30;
-   }
+  // making sure the destination is inputed
+  if (destination === "") {
+    alert("Please select a destination.");
+    return;
+  }
 
-   // Add overweight fee if weight > 5kg
-   if (weight > 5) {
-      baseCost += 15;
-   }
+  // starting price
+  let baseCost = 4000; 
 
-   // Apply express surcharge
-   if (expressDelivery) {
-      baseCost *= 1.5;
-   }
+  // Add cost based on destination
+  if (destination === "aba") {
+   // if the destination is in and around aba no extracharges
+  } else if (destination === "bende") {
+    baseCost += 2000;
+  } else if (destination === "ohafia") {
+    baseCost += 3000;
+  } else if (destination === "ukwa-east") {
+    baseCost += 1000;
+  } else if (destination === "ukwa-west") {
+    baseCost += 1500;
+  } else if (destination === "umuahia") {
+    baseCost += 2500;
+  }
 
-   // Display result
-   document.getElementById("total-amount").innerText = "₦" + baseCost.toFixed(2);
-   console.log(`The total shipping cost for your ${weight}kg package to ${destination} is ₦${baseCost}`);
+  // Overweight fee: every 1kg above 5kg
+  if (weight > 5) {
+    let extraKg = weight - 5;
+    baseCost += extraKg * 50; 
+  }
+
+  // Apply extra charges for express delivery
+  if (expressDelivery) {
+    baseCost *= 1.5;
+  }
+
+  // Display result
+  document.getElementById("total-amount").innerText = "₦" + baseCost.toFixed(2);
 }
